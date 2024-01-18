ee028122c6866fffad50a60fb227d4ddeda21e54
jun_ <gurwns232@naver.com>
2024-01-18 17:07:54 +0900

    feat: js challenge 16 - icecream

---
+++ challenge-js/16-review/icecream/app.js
@@ -0,0 +1,33 @@
+const $add = document.querySelector('.add')
+const $print = document.querySelector('.print')
+const $list = document.querySelector('.list')
+const $container = document.querySelector('.container')
+
+let list = ['메로나']
+
+function listToText() {
+  $container.textContent = list.join(', ')
+}
+
+function printIce() {
+  list.forEach((ice) => alert(ice))
+}
+
+function addIce() {
+  const ice = prompt('추가하고 싶은 아이스크림의 이름을 입력해주세요')
+  if (ice === '') return
+  if (list.includes(ice)) {
+    alert('이미 있는 아이스')
+    return
+  }
+  list.push(ice)
+}
+
+function init() {
+  listToText()
+  $add.addEventListener('click', addIce)
+  $print.addEventListener('click', printIce)
+  $list.addEventListener('click', listToText)
+}
+
+init()
