60f4cd6025460415c87fabd2dcf1bc1d7b947762
jun_ <gurwns232@naver.com>
2024-02-13 18:07:27 +0900

    refactor: 함수형 컴포넌트로 변경

--- challenge-react/crud-app/src/App.js
+++ challenge-react/crud-app/src/App.js
@@ -1,47 +1,39 @@
-import { Component } from 'react'
+import { Component, useState } from 'react'
 import './App.css'
 import ExpenseForm from './components/ExpenseForm'
 import ExpenseList from './components/ExpenseList'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      expenses: [
-        { id: 1, charge: '콜라', amount: 2000 },
-        { id: 2, charge: '빵', amount: 1000 },
-        { id: 3, charge: '맥북', amount: 20000 },
-      ],
-    }
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, charge: '콜라', amount: 2000 },
+    { id: 2, charge: '빵', amount: 1000 },
+    { id: 3, charge: '맥북', amount: 20000 },
+  ])
+
+  const handleDelete = (id) => {
+    const newExpense = expenses.filter((expense) => expense.id !== id)
+    setExpenses(newExpense)
   }
 
-  handleDelete = (id) => {
-    const newExpense = this.state.expenses.filter((expense) => expense.id !== id)
-    this.setState({ expenses: newExpense })
-  }
+  return (
+    <main className="main-container">
+      <div className="sub-container">
+        <h1>장바구니</h1>
 
-  render() {
-    return (
-      <main className="main-container">
-        <div className="sub-container">
-          <h1>장바구니</h1>
-
-          <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-            <ExpenseForm />
-          </div>
-
-          <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-            <ExpenseList
-              initialExpenses={this.state.expenses}
-              handleDelete={this.handleDelete}
-            />
-          </div>
-
-          <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
-            <p style={{ fontSize: '2rem' }}>총합계: </p>
-          </div>
+        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+          <ExpenseForm />
         </div>
-      </main>
-    )
-  }
+
+        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+          <ExpenseList initialExpenses={expenses} handleDelete={handleDelete} />
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'start', marginTop: '1rem' }}>
+          <p style={{ fontSize: '2rem' }}>총합계: </p>
+        </div>
+      </div>
+    </main>
+  )
 }
+
+export default App
--- challenge-react/crud-app/src/components/ExpenseList.js
+++ challenge-react/crud-app/src/components/ExpenseList.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react'
 import './ExpenseList.css'
 import ExpenseItem from './ExpenseItem'
 
-export default class ExpenseList extends Component {
-  render() {
-    return (
-      <>
-        <ul className="list">
-          {this.props.initialExpenses.map((expense, index) => {
-            return (
-              <ExpenseItem
-                key={index}
-                expense={expense}
-                handleDelete={this.props.handleDelete}
-              />
-            )
-          })}
-        </ul>
-        <button className="btn">목록 지우기</button>
-      </>
-    )
-  }
+const ExpenseList = ({ initialExpenses, handleDelete }) => {
+  return (
+    <>
+      <ul className="list">
+        {initialExpenses.map((expense, index) => {
+          return <ExpenseItem key={index} expense={expense} handleDelete={handleDelete} />
+        })}
+      </ul>
+      <button className="btn">목록 지우기</button>
+    </>
+  )
 }
+
+export default ExpenseList
