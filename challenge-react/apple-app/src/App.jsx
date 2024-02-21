9104424321223b5f29e57a941e684f8d27ac4fba
jun_ <gurwns232@naver.com>
2024-02-21 11:32:58 +0900

    feat: apple tv, row 컴포넌트 추가

--- challenge-react/apple-app/src/App.jsx
+++ challenge-react/apple-app/src/App.jsx
@@ -2,12 +2,18 @@ import styled from 'styled-components'
 import './App.css'
 import Nav from './components/Nav'
 import Banner from './components/Banner'
+import Row from './components/Row'
+import requests from './api/request'
 
 function App() {
   return (
     <Container>
       <Nav />
       <Banner />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   )
 }
--- challenge-react/apple-app/src/components/Row.jsx
+++ challenge-react/apple-app/src/components/Row.jsx
@@ -1,7 +1,43 @@
+import { useCallback, useEffect, useState } from 'react'
 import './Row.css'
+import axios from '../api/axios'
 
-const Row = () => {
-  return <div>Row</div>
+// eslint-disable-next-line react/prop-types
+const Row = ({ title, id, fetchUrl }) => {
+  const [moives, setMoives] = useState([])
+
+  const fetchMovieData = useCallback(async () => {
+    const response = await axios.get(fetchUrl)
+    setMoives(response.data.results)
+  }, [fetchUrl])
+
+  useEffect(() => {
+    fetchMovieData()
+  }, [fetchMovieData])
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{'<'}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {moives?.map((movie) => (
+            <img
+              key={moives.id}
+              className="row__poster"
+              src={`https:image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={moives.name}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{'>'}</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Row
