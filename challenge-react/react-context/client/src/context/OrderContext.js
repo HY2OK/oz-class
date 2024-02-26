c87e8fd9838e12395a3e34dfa9a436fab7013675
jun_ <gurwns232@naver.com>
2024-02-26 14:48:42 +0900

    feat: 상품 count를 이용한 가격 계산

--- challenge-react/react-context/client/src/context/OrderContext.js
+++ challenge-react/react-context/client/src/context/OrderContext.js
@@ -1,12 +1,43 @@
-import { createContext, useMemo, useState } from 'react'
+import { createContext, useEffect, useMemo, useState } from 'react'
 
-const OrderContext = createContext()
+export const OrderContext = createContext()
 
 export function OrderContextProvider(props) {
   const [orderCounts, setOrderCounts] = useState({
     products: new Map(),
     options: new Map(),
   })
+
+  const [totals, setTotals] = useState({
+    products: 0,
+    options: 0,
+    total: 0,
+  })
+
+  const pricePerItem = {
+    products: 1000,
+    options: 500,
+  }
+
+  const calculateSubtotal = (orderType, orderCounts) => {
+    let optionCount = 0
+    for (const count of orderCounts[orderType].values()) {
+      optionCount += count
+    }
+    return optionCount * pricePerItem[orderType]
+  }
+
+  useEffect(() => {
+    const productsTotal = calculateSubtotal('products', orderCounts)
+    const optionsTotal = calculateSubtotal('options', orderCounts)
+    const total = productsTotal + optionsTotal
+
+    setTotals({
+      products: productsTotal,
+      options: optionsTotal,
+      total: total,
+    })
+  }, [orderCounts])
 
   const value = useMemo(() => {
     function updateItemCount(itemName, newItemCount, orderType) {
@@ -17,8 +48,8 @@ export function OrderContextProvider(props) {
       setOrderCounts(newOderCounts)
     }
 
-    return [{ ...orderCounts }, updateItemCount]
-  }, [orderCounts])
+    return [{ ...orderCounts, totals }, updateItemCount]
+  }, [orderCounts, totals])
 
-  return <OrderContext.Provider value {...props} />
+  return <OrderContext.Provider value={value} {...props} />
 }
