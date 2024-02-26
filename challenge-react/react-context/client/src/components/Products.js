00d427f02638c206759c2d86349ad4e436b8941d
jun_ <gurwns232@naver.com>
2024-02-26 12:04:54 +0900

    feat: 상품 이미지 가져오기

--- challenge-react/react-context/client/src/components/Products.js
+++ challenge-react/react-context/client/src/components/Products.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
-const Products = () => {
-  return <div>Products</div>
+const Products = ({ name, imagePath }) => {
+  console.log(name, imagePath)
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <img
+        src={`http://localhost:4000/${imagePath}`}
+        alt={`${name} product`}
+        style={{ width: '75%' }}
+      />
+      <form style={{ marginTop: '10px' }}>
+        <label style={{ textAlign: 'right' }}>{name}</label>
+        <input
+          type="number"
+          style={{ marginLeft: '7px' }}
+          name="quantity"
+          min="0"
+          defaultValue={0}
+        />
+      </form>
+    </div>
+  )
 }
 
 export default Products
--- challenge-react/react-context/client/src/components/Type.js
+++ challenge-react/react-context/client/src/components/Type.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Products from './Products'
+import Options from './Options'
 
 const Type = ({ orderType }) => {
-  console.log('orderType', orderType)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    loadItems(orderType)
+  }, [orderType])
+
+  const loadItems = async () => {
+    try {
+      const response = await axios.get(`http://localhost:4000/${orderType}`)
+      setItems(response.data)
+    } catch (error) {}
+  }
+
+  const ItemComponent = orderType === 'products' ? Products : Options
+
+  const optionItems = items.map((item) => (
+    <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
+  ))
+
   return (
     <div>
       <h2>주문 종류</h2>
@@ -13,7 +34,7 @@ const Type = ({ orderType }) => {
           flexDirection: orderType === 'options' ? 'column' : 'row',
         }}
       >
-        itmes
+        {optionItems}
       </div>
     </div>
   )
