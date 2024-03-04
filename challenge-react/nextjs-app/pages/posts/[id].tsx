c5e9ee60fd7b318f9469f6929eb33d0a1cc3a8df
jun_ <gurwns232@naver.com>
2024-03-04 12:03:02 +0900

    feat: post 데이터 가져와서 보여주기

--- challenge-react/nextjs-app/pages/posts/[id].tsx
+++ challenge-react/nextjs-app/pages/posts/[id].tsx
@@ -1,5 +1,46 @@
-const Post = () => {
-  return <div>Post</div>
+import { getAllPostsIds, getPostData, getSortedPostsData } from '@/lib/post'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={styles.headingXl}>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
+    </div>
+  )
 }
 
 export default Post
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostsIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
