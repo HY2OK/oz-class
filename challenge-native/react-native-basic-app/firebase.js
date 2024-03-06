98b5a241ae112077a1e0b63e4e5fc720d6ea0ae3
jun_ <gurwns232@naver.com>
2024-03-06 11:47:00 +0900

    feat: firebase를 이용한 회원 가입

---
+++ challenge-native/react-native-basic-app/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDqHZJdLrKWK4wwml0qBlWKse9Cbe_WLtU',
+  authDomain: 'react-native-todo-app-37d34.firebaseapp.com',
+  projectId: 'react-native-todo-app-37d34',
+  storageBucket: 'react-native-todo-app-37d34.appspot.com',
+  messagingSenderId: '51483202957',
+  appId: '1:51483202957:web:4a33d002b519497ed859c4',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export default app
