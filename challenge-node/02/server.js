b177086af8bd2ba197f9f3e67094565a182b76cf
jun_ <gurwns232@naver.com>
2024-03-21 16:11:27 +0900

    feat: node - mvc 패턴으로 작성

---
+++ challenge-node/02/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const usersController = require('./controllers/users.controller')
+const postsController = require('./controllers/posts.controller')
+
+const PORT = 4000
+
+const app = express()
+app.use(express.json())
+
+app.use((req, res, next) => {
+  const start = Date.now()
+  console.log(`${req.method} ${req.url}`)
+  next()
+  const diffTime = Date.now() - start
+  console.log(`end: ${req.method} ${req.url} ${diffTime}ms`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.get('/users', usersController.getUsers)
+app.get('/users/:userId', usersController.getUser)
+app.post('/users', usersController.postUser)
+
+app.get('/posts', postsController.getPost)
+
+app.listen(PORT, () => {
+  console.log(`Running on port http://localhost:${PORT}`)
+})
