88e3d4f0fb9adc39896433efb372d807ef243b3d
jun_ <gurwns232@naver.com>
2024-03-21 16:39:55 +0900

    feat: node - router

--- challenge-node/02/server.js
+++ challenge-node/02/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const usersController = require('./controllers/users.controller')
-const postsController = require('./controllers/posts.controller')
+const usersRouter = require('./routes/users.router.js')
+const postsRouter = require('./routes/posts.router.js')
 
 const PORT = 4000
 
@@ -12,18 +12,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`)
   next()
   const diffTime = Date.now() - start
-  console.log(`end: ${req.method} ${req.url} ${diffTime}ms`)
+  console.log(`end: ${req.method} ${req.baseUrl}${req.url} ${diffTime}ms`)
 })
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.get('/users', usersController.getUsers)
-app.get('/users/:userId', usersController.getUser)
-app.post('/users', usersController.postUser)
-
-app.get('/posts', postsController.getPost)
+app.use('/users', usersRouter)
+app.use('/posts', postsRouter)
 
 app.listen(PORT, () => {
   console.log(`Running on port http://localhost:${PORT}`)
