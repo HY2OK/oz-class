3817feb5be24aedc6fe21a577888f520a1fc4d3a
jun_ <gurwns232@naver.com>
2024-03-21 17:07:17 +0900

    feat: node - Template Layout

--- challenge-node/02/controllers/posts.controller.js
+++ challenge-node/02/controllers/posts.controller.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
 function getPost(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'mountain.webp'))
+  res.render('posts', {
+    templateName: 'post',
+  })
 }
 
 module.exports = { getPost }
