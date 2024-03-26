3a05a3449bb2369fede28faab7f412f9080f31cf
jun_ <gurwns232@naver.com>
2024-03-26 14:45:23 +0900

    feat: 로그인/회원가입 위한 화면 구현(ejs)

--- challenge-node/04-express-passport-app/src/server.js
+++ challenge-node/04-express-passport-app/src/server.js
@@ -4,6 +4,10 @@ const path = require('path')
 const mongodbKey = require('../config/key')
 const app = express()
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
@@ -18,6 +22,14 @@ mongoose
   })
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/signup', (req, res) => {
+  res.render('signup')
+})
 
 const port = 4000
 app.listen(port, () => {
